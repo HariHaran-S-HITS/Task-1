c4772b4c2f69d7a93be5120b159111b626e5f6ba
AIGAMER <hariharankvasn@gmail.com>
2021-08-12 17:00:06 +0530

    Updates

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.UserList.as_view()),
+    path('users/', views.UserView.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
 ]
 
